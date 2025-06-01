185201a81538ce26c4013770db647cbf72d1d1ac
arun5707 <aruniasks@gmail.com>
2025-06-01 21:42:41 +0530

    Add files via upload

---
+++ Day - 1/to do list.py
@@ -0,0 +1,17 @@
+#1. Add
+#What it does: Lets the user type in a new task.
+#Example: If you type add, the app will ask, “What is your task?” You type: “Buy milk.” The task is saved.
+
+#2. View
+#What it does: Shows all the tasks in a list format.
+#Example Output:
+#markdown
+#Copy
+#Edit
+#Your To-Do List:
+#1. Buy milk
+#2. Finish homework
+
+#3. Remove
+#What it does: Lets the user delete a task by selecting its number.
+#How it works: The app shows the tasks (like above). You type the number of the task you want to delete.
